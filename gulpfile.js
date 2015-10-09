3200f16b9a167d9bbf1132e08b93d8e07524c2b3
Jonathan Webb <jonathan@webbdev.ca>
2015-10-08 23:07:45 -0400

    updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,14 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var htmlmin = require('gulp-htmlmin');
 var rename = require("gulp-rename");
+var concat = require('gulp-concat');
+
+gulp.task('min', function() {
+	return gulp.src("scripts.js")
+		.pipe(uglify())
+		.pipe(rename("scripts.min.js"))
+		.pipe(gulp.dest("./dist/"))
+});
 
 gulp.task('build', function() {
 	return gulp.src('./_index.html')
